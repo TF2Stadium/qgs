c29970bdd4b3cf4f41129a3409a96e445480dc78
gpittarelli <g@gjp.cc>
2017-09-16 23:01:24 -0700

    Get server start+stopping actually working

--- backend/src/queries/server.js
+++ backend/src/queries/server.js
@@ -1,3 +1,9 @@
-export const getStatus = (db, serverId) => db.query(`
+import {one} from '../dbHelpers';
+
+export const getServer = (db, serverId) => one(db.query(`
+SELECT * FROM server WHERE id=$1
+`, [serverId]));
+
+export const getStatus = (db, serverId) => one(db.query(`
 SELECT status FROM server WHERE id=$1
-`, [serverId]);
+`, [serverId]));
--- backend/src/systems/jobqueue.js
+++ backend/src/systems/jobqueue.js
@@ -1,20 +1,48 @@
+import {sample} from 'lodash';
+import {take} from 'ramda';
 import {createService} from 'ineedthis';
 import dbService from './db';
 import configuration from './config';
 import gceService from './gce';
 import PgBoss from 'pg-boss';
 import {txPool} from '../dbHelpers';
-import {getStatus} from '../queries/server';
+import {statuses, locationsByName} from '../constants';
+import {getServer} from '../queries/server';
 import debugLib from 'debug';
 const debug = debugLib('jobqueue');
 
 const setStatus = `UPDATE server SET status=$1
 WHERE owner_id=$2 and id=$3`;
 
+const startup = `UPDATE server
+SET status='starting', instance=$1
+WHERE owner_id=$2 and id=$3`;
+
+const stop = `UPDATE server
+SET status='stopping', instance=null
+WHERE owner_id=$1 and id=$2`;
+
+function job(fn) {
+  return async (job) => {
+    let result,
+      error;
+    try {
+      result = await fn(job);
+    } catch (e) {
+      console.error(e);
+      error = e;
+    }
+    job.done(error, result);
+  };
+}
+
 export default createService('qgs/jobqueue', {
   dependencies: [dbService, configuration, gceService],
   start: () => async ({
-    [configuration.serviceName]: {jobqueuePostgres: postgresConfig},
+    [configuration.serviceName]: {
+      jobqueuePostgres: postgresConfig,
+      gce: {tf2Image},
+    },
     [gceService.serviceName]: gce,
     [dbService.serviceName]: pool,
   }) => {
@@ -22,34 +50,81 @@ export default createService('qgs/jobqueue', {
     const queue = new PgBoss(postgresConfig);
     await queue.start();
 
-    await queue.subscribe('server/start', {}, async (job) => {
-      await db.query(setStatus, ['starting', by, serverId]);
+    await queue.subscribe('server/start', {}, job(async (job) => {
+      const {by, serverId} = job.data,
+        server = await getServer(pool, serverId);
 
-      const {by, serverId} = job.data;
-      await db.query(setStatus, ['starting', by, serverId]);
+      if (server.status !== statuses.stopped) {
+        throw new Error('Only stopped servers can be started');
+      }
 
-          await new Promise(r=>setTimeout(()=>r(), 10000));
-          await db.query(setStatus, ['running', by, serverId]);
-        } catch (e) {
-          console.error(e);
-        }
-        job.done();
-      });
-    });
+      const name = `tf2-server-${take(8, (Math.random()+'').split('.')[1])}`,
+        zoneName = sample(locationsByName[server.location].zones),
+        zone = gce.zone(zoneName);
 
-    await queue.subscribe('server/stop', {}, job => {
-      txPool(pool, async (db) => {
-        try {
-          const {by, serverId} = job.data;
-          await db.query(setStatus, ['stopping', by, serverId]);
-          await new Promise(r=>setTimeout(()=>r(), 10000));
-          await db.query(setStatus, ['stopped', by, serverId]);
-        } catch (e) {
-          console.error(e);
-        }
-        job.done();
-      });
-    });
+      await pool.query(startup, [`gce:${zoneName}/${name}#1`, by, serverId]);
+
+      debug(`Launching VM ${name} in ${zoneName} with ${tf2Image}`);
+
+      try {
+        await zone.createVM(name, {
+          machineType: 'n1-standard-2',
+          disks: [
+            {
+              boot: true,
+              initializeParams: {
+                sourceImage: tf2Image
+              }
+            }
+          ],
+          networkInterfaces: [{
+            accessConfigs: [{
+              type: "ONE_TO_ONE_NAT",
+              name: "External NAT"
+            }],
+            network: 'global/networks/default'
+          }]
+        });
+      } catch (e) {
+        await pool.query(setStatus, ['stopped', null, serverId]);
+        throw e;
+      }
+
+      await new Promise(r=>setTimeout(()=>r(), 10000));
+      await pool.query(setStatus, ['running', by, serverId]);
+    }));
+
+    await queue.subscribe('server/stop', {}, job(async (job) => {
+      const {by, serverId} = job.data,
+        server = await getServer(pool, serverId);
+
+      if (server.status !== statuses.running) {
+        throw new Error('Only running servers can be stopped');
+      }
+
+      const [provider, loc] = server.instance.split(':'),
+        [zoneName, serverName] = loc.split('/'),
+        [instanceId, slot=1] = serverName.split('#');
+
+      const zone = gce.zone(zoneName),
+        vm = zone.vm(instanceId);
+      debug(`Killing VM ${instanceId} in ${zoneName}`);
+
+      let exists = false;
+      try {
+        await vm.get();
+        exists = true;
+      } catch (e) {
+        // Server already gone! (possibly a retry of this event)
+      }
+      if (exists) {
+        await vm.delete();
+      }
+
+      await pool.query(stop, [by, serverId]);
+      await new Promise(r=>setTimeout(()=>r(), 1000));
+      await pool.query(setStatus, ['stopped', by, serverId]);
+    }));
 
     debug('Started');
     return queue;
