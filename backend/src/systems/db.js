f01c0426892d6a892e40673fa2f5b271a85afc04
gpittarelli <g@gjp.cc>
2017-09-16 22:58:41 -0700

    Log db start/stopping

--- backend/src/systems/db.js
+++ backend/src/systems/db.js
@@ -1,18 +1,26 @@
 import {createService} from 'ineedthis';
 import configuration from './config';
 import {Pool} from 'pg';
+import debugLib from 'debug';
+const debug = debugLib('db');
 
 export default createService('qgs/db', {
   dependencies: [configuration],
   start: () => async ({
     [configuration.serviceName]: {postgres: postgresConfig},
   }) => {
+    debug('Starting...');
     const pool = new Pool(postgresConfig);
 
     // Test connection + get 1 client connected
     await pool.query('SELECT 1+1;');
 
+    debug('Started');
     return pool;
   },
-  stop: (pool) => pool.end(),
+  async stop(pool) {
+    debug('Stopping...');
+    await pool.end();
+    debug('Stopped');
+  }
 });
